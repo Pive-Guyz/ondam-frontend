971b2a1ab246bb8f5946784c51d0c305354b94f0
MJay1123 <tjalswhd1@naver.com>
2025-04-25 18:12:31 +0900

    기초 세팅 + 메인페이지(간단 구현)

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+// 메인페이지
+import MainPage from '@/views/MainPage.vue'
+import DiaryPage from '../views/DiaryPage.vue'
+
+const routes = [
+  { path: '/', component: MainPage},
+  { path: '/diary', component: DiaryPage}
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+  })
+
+export default router
