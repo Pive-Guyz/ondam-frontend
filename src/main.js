0a504ce96d49f157579ce08dfbbce79876480c92
Morris235 <dltkdah777@gmail.com>
2025-04-27 15:39:13 +0900

    chore : main.js에 부트스트랩 등록

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
-import './assets/main.css'
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 
 // createApp(App).mount('#app')
@@ -11,4 +13,5 @@ const app = createApp(App)
 
 app.use(createPinia()) // Pinia 사용 등록
 app.use(router)
+app.use(BootstrapVue3)
 app.mount('#app')
