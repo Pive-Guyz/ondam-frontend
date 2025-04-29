350400886ae24d089df5b3d83eacd98eb4729e23
HMYIEN <bsm0407@naver.com>
2025-04-29 18:49:12 +0900

    refactor: 오류 수정

--- src/main.js
+++ src/main.js
@@ -2,26 +2,24 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
-import vuetify from './plugins/vuetify'  // ✅ 추가!
 import { useAuthStore } from '@/stores/auth'
 
-import 'vuetify/styles'  // ✅ Vuetify 스타일
+import 'vuetify/styles' // Vuetify 기본 스타일
+import '@mdi/font/css/materialdesignicons.css' // ✅ MDI 아이콘 추가
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import '@mdi/font/css/materialdesignicons.css'
 
 const vuetify = createVuetify({
-    components,
-    directives,
-    icons: {
-        defaultSet: 'mdi',
-      },
-  })
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi', // ✅ MDI 아이콘 세트 사용 설정
+  },
+})
 
 const app = createApp(App)
 const pinia = createPinia()
-
 
 app.use(pinia)
 app.use(router)
@@ -30,5 +28,3 @@ app.mount('#app')
 
 const authStore = useAuthStore()
 authStore.restoreSession()  // 로그인 상태 복원!
-
-
